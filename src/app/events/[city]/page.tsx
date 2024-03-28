eb471a9af3889826f9d9c19dc7691277c4c0c3c6
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-28 16:45:17 -0700

    - navigate to home page by clicking on evento logo

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -2,7 +2,7 @@ import { HeadingH1 } from "@/components/heading-h1";
 
 export default function EventsPage() {
   return (
-    <main>
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <HeadingH1>Events in Austin</HeadingH1>
     </main>
   );
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Image
-      alt="EVENTO logo"
-      src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
-      height={12}
-      width={53}
-    />
+    <Link href="/">
+      <Image
+        alt="EVENTO logo"
+        src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+        height={12}
+        width={53}
+      />
+    </Link>
   );
 }
