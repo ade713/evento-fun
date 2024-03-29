c5d2a67b42101f868e5e83e915d63a2b53c8d9d8
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-28 17:11:42 -0700

    - change page heading based on page params

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,21 @@
 import { HeadingH1 } from "@/components/heading-h1";
 
-export default function EventsPage() {
+type EventsPageProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default function EventsPage({params}: EventsPageProps) {
+  const city = params.city;
+  const capitalizedCity = city[0].toUpperCase() + city.slice(1);
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      <HeadingH1>Events in Austin</HeadingH1>
+      <HeadingH1>
+        { city === 'all' && 'All Events' }
+        { city !== 'all' && `Events in ${capitalizedCity}` }
+      </HeadingH1>
     </main>
   );
 }
