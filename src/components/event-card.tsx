d94fce0fa47513d6d32da3105092b479db9981fa
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-28 19:55:59 -0700

    - start adding elements and styling to EventCard, e.g. image, eventi info

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,4 +1,5 @@
 import { EventoEvent } from "@/lib/types";
+import Image from "next/image";
 
 type EventCardProps = {
   event: EventoEvent;
@@ -6,6 +7,21 @@ type EventCardProps = {
 
 export function EventCard({ event }: EventCardProps) {
   return (
-    <section>{event.name}</section>
+    <section
+      className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden"
+    >
+      <Image
+        src={event.imageUrl}
+        alt={event.name}
+        width={500}
+        height={280}
+        className="h-[60%] object-fill"
+      />
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <h2 className="text-2xl font-semibold">{event.name}</h2>
+        <p className="italic text-white/75">By {event.organizerName}</p>
+        <p className="text-sm text-white/50 mt-4">By {event.location}</p>
+      </div>
+    </section>
   );
 }
