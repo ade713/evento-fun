3af532cc4dacc7df211bff6e3fb8301febe82f61
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-28 23:50:35 -0700

    - use city name to fetch events for respective city

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -9,13 +9,13 @@ type EventsPageProps = {
 };
 
 export default async function EventsPage({params}: EventsPageProps) {
-  const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/evento/api/events/?city=austin"
-  );
-  const events: EventoEvent[] = await response.json();
-
   const city = params.city;
   const capitalizedCity = city[0].toUpperCase() + city.slice(1);
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/?city=${city}`
+  );
+  const events: EventoEvent[] = await response.json();
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
