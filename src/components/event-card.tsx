3cb28a1087b1701bcd9e408532245e5de9b7e884
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-31 00:10:12 -0700

    - add transformation effects to enlarge event cards to standard size as they come into view port

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,16 +1,41 @@
+"use client";
+
 import { EventoEvent } from "@/lib/types";
+import { motion, useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 
 type EventCardProps = {
   event: EventoEvent;
 };
 
+const MotionLink = motion(Link);
+
 export function EventCard({ event }: EventCardProps) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.5 1"],
+  });
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
+
   return (
-    <Link
+    <MotionLink
       className="flex-1 basis-80 h-[380px] max-w-[500px]"
       href={`/event/${event.slug}`}
+      ref={ref}
+      style={{
+        // @ts-ignore
+        opacity: opacityProgress,
+        // @ts-ignore
+        scale: scaleProgress,
+      }}
+      initial={{
+        opacity: 0,
+        scale: 0.8,
+      }}
     >
       <section
         className="w-full h-full flex flex-col bg-white/[3%] rounded-xl overflow-hidden relative state-effects"
@@ -41,6 +66,6 @@ export function EventCard({ event }: EventCardProps) {
           </p>
         </section>
       </section>
-    </Link>
+    </MotionLink>
   );
 }
