737287b4f9855d51c9c3242dd02ac3e596798226
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-03-30 23:11:18 -0700

    - extract event(s) fetching to utility functions

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { HeadingH1 } from "@/components/heading-h1";
-import { capitalizeWord } from "@/lib/utils";
+import { getEvent } from "@/lib/utils";
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -11,11 +11,7 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const slug = params.slug;
-
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  const event = await response.json();
+  const event = await getEvent(slug);
 
   return {
     title: event.name,
@@ -24,10 +20,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function EventPage({ params }: Props) {
   const slug = params.slug;
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  const event = await response.json();
+  const event = await getEvent(slug);
 
   return (
     <main>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { EventoEvent } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,3 +9,21 @@ export function cn(...inputs: ClassValue[]) {
 export function capitalizeWord(word: string): string {
   return word[0].toUpperCase() + word.slice(1);
 };
+
+export async function getEvents(city: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/?city=${city}`
+  );
+  const events: EventoEvent[] = await response.json();
+
+  return events;
+}
+
+export async function getEvent(slug: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+
+  return event;
+}
