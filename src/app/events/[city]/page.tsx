de4c7016292cb4af577245bd1c61f36e09309d9c
Ade Farquhar <adefarquhar@Ades-MacBook-Air.local>
2024-04-02 17:41:52 -0700

    - validate page number to prevent handle errors with url page params   - use zod for validation of page number

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -4,6 +4,7 @@ import { Suspense } from "react";
 import Loading from "./loading";
 import { Metadata } from "next";
 import { capitalizeWord } from "@/lib/utils";
+import { z } from "zod";
 
 type Props = {
   params: {
@@ -25,10 +26,17 @@ export function generateMetadata({ params }: Props): Metadata {
   };
 };
 
+const pageNumberSchema = z.coerce.number().int().positive().optional();
+
 export default async function EventsPage({ params, searchParams}: EventsPageProps) {
   const city = params.city;
   const capitalizedCity = capitalizeWord(city);
-  const page = searchParams.page || 1;
+  const parsedPage = pageNumberSchema.safeParse(searchParams.page);
+  console.log({parsedPage})
+
+  if (!parsedPage.success) {
+    throw new Error("invalid page number");
+  }
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -37,8 +45,8 @@ export default async function EventsPage({ params, searchParams}: EventsPageProp
         { city !== 'all' && `Events in ${capitalizedCity}` }
       </HeadingH1>
 
-      <Suspense key={city + page} fallback={<Loading />}>
-        <EventsList city={city} page={+page} />
+      <Suspense key={city + parsedPage.data} fallback={<Loading />}>
+        <EventsList city={city} page={parsedPage.data} />
       </Suspense>
     </main>
   );
